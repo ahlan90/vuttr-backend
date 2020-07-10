6668a089ee3574ed059a3a806ffee4ac2619e0e4
Ahlan Guarnier <ahlan90@gmail.com>
2020-07-10 14:15:09 -0300

    Adding config for local database url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ToolsModule } from './tools/tools.module';
+import { dataBaseURL } from 'local.settings';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(process.env.MONGO_URL || dataBaseURL),
     ToolsModule,
   ]
 })
