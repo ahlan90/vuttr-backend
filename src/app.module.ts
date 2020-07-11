d7eafb943dfa5a9dce0ba729a8dedc6bc149fa6c
Ahlan <ahlan90@gmail.com>
2020-07-11 11:14:13 -0300

    Fix the enviroments local variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ToolsModule } from './tools/tools.module';
-import { dataBaseURL } from 'local.settings';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URL || dataBaseURL),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     ToolsModule,
     AuthModule
   ],
